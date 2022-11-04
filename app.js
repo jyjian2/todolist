727f42898d81e97d47a4b5f0d48d9d7ca98b8dde
Jia-Yi Jian <jyjian@illinois.edu>
2022-11-03 17:42:34 -0700

    add keep reading user input functionality

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = ["Clean house", "Web course", "Leetcode"];
+var newItem = "";
+
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extension:true}));
@@ -12,15 +15,14 @@ app.get("/", function(req, res) {
   const options = { weekday: "long", year: "numeric", month: "long", day: "numeric"};
   var dayValue = today.toLocaleDateString("en-US", options);
 
-  res.render("list", {
-    passDay: dayValue
-  })
+  res.render("list", {passDay:dayValue, userAddItems: items})
 
 });
 
 app.post("/", function(req, res) {
-  var newItem = req.body.newItem;
-  console.log(newItem);
+  newItem = req.body.newItem;
+  items.push(newItem);
+  res.redirect("/");
 })
 app.listen(3000, function() {
   console.log("all good");
