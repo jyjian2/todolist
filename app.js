d1f4141341df60d379c466dc843fc0e89d583c87
Jia-Yi Jian <jyjian@illinois.edu>
2022-11-20 17:45:26 -0800

    connect to mongoDB and create item collection

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
 const mongoose = require("mongoose");
 
 const app = express();
 
-const items = ["Clean house", "Web course", "Leetcode"];
 let newItem = "";
 const workItems = [];
 
@@ -14,13 +12,35 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extension:true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://120.0.0.7:27017/todolistDB");
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB");
 
 const itemSchema = {
   name: String
 };
 
 const Item = mongoose.model("Item", itemSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todolist!"
+});
+
+const item2 = new Item({
+  name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, function(err){
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Save default items to DB");
+  }
+});
 
 app.get("/", function(req, res) {
 
